8bb2cee695678a10a21c0cac212c682540100d89
whitejenkins <Max234576184@gmail.com>
2026-04-17 16:07:27 +0300

    Add Statistic

--- SimpleSSHLogAnalyzer.py
+++ SimpleSSHLogAnalyzer.py
@@ -11,16 +11,25 @@ def main(file):
 
     with open(file, "r", encoding="utf-8") as openfile:
         lines = openfile.readlines()
+        successful = 0
+        failed = 0
 
         print('Successful:')
         for line in lines:
             if "Successful login" in line:
                 print(line.strip())
+                successful += 1
+        
 
-        print('Failed:')
+        print('\nFailed:')
         for line in lines:
             if "Failed login" in line:
                 print(line.strip())
+                failed += 1
+
+        print('\nStatistic:')
+        print(f'Number of successful attempts: {successful}')
+        print(f'Number of failed attempts: {failed}')
 
 if __name__ == '__main__':
     main()
